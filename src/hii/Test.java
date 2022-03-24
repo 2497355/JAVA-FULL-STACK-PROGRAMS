52ba77de094e7230fce4f72570f3e958f077dcf5
Vands11 <divyavandana9095@gmail.com>
2022-03-24 16:26:57 +0530

    test

---
+++ src/hii/Test.java
@@ -0,0 +1,18 @@
+package hii;
+
+
+public class Test {
+	   public void pupAge() {
+		     int age=5;
+		      age = age + 7;
+		      
+		   
+		    
+		      System.out.println("Puppy age is : " + age);
+		   }
+
+		   public static void main(String args[]) {
+		      Test test = new Test();
+		      test.pupAge();
+		   }
+		}
