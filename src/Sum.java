3e9f7d51055980683cf935d939ed057d2d0d4510
Vands11 <divyavandana9095@gmail.com>
2022-03-24 17:07:43 +0530

    h

---
+++ src/Sum.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public Sum() {
+		// TODO Auto-generated constructor stub
+		Scanner a = new Scanner(System.in);
+		int add ;
+		System.out.println("Enter the first No:");
+	      int x = a.nextInt();
+		System.out.println("Enter the Second No:");
+		int y = a.nextInt();
+		add =x+y;
+		System.out.println(add);
+	}
+
+}
+
