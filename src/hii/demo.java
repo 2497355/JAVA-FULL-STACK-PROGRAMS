a66fd2f01ad9eb26940918a157a3fc84c102bb5c
Vands11 <divyavandana9095@gmail.com>
2022-03-24 17:05:38 +0530

    thank you

---
+++ src/hii/demo.java
@@ -0,0 +1,19 @@
+package hii;
+
+public class demo {
+
+	
+
+		public static void main (String args [] )
+
+		{
+
+		int a=5;
+
+		System.out.println("Variable value is  "+ a );
+
+		}
+
+		    }
+
+
